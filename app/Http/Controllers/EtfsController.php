b89b87d38562f66b2bfb1102af158b8e5715be0b
asnike <ruudnike@gmail.com>
2018-03-18 19:39:30 +0900

    * 테이블 수정 * 시더 추가

---
+++ app/Http/Controllers/EtfsController.php
@@ -0,0 +1,85 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use App\Etf;
+use Illuminate\Http\Request;
+
+class EtfsController extends Controller
+{
+    /**
+     * Display a listing of the resource.
+     *
+     * @return \Illuminate\Http\Response
+     */
+    public function index()
+    {
+        //
+    }
+
+    /**
+     * Show the form for creating a new resource.
+     *
+     * @return \Illuminate\Http\Response
+     */
+    public function create()
+    {
+        //
+    }
+
+    /**
+     * Store a newly created resource in storage.
+     *
+     * @param  \Illuminate\Http\Request  $request
+     * @return \Illuminate\Http\Response
+     */
+    public function store(Request $request)
+    {
+        //
+    }
+
+    /**
+     * Display the specified resource.
+     *
+     * @param  \App\Etf  $etf
+     * @return \Illuminate\Http\Response
+     */
+    public function show(Etf $etf)
+    {
+        //
+    }
+
+    /**
+     * Show the form for editing the specified resource.
+     *
+     * @param  \App\Etf  $etf
+     * @return \Illuminate\Http\Response
+     */
+    public function edit(Etf $etf)
+    {
+        //
+    }
+
+    /**
+     * Update the specified resource in storage.
+     *
+     * @param  \Illuminate\Http\Request  $request
+     * @param  \App\Etf  $etf
+     * @return \Illuminate\Http\Response
+     */
+    public function update(Request $request, Etf $etf)
+    {
+        //
+    }
+
+    /**
+     * Remove the specified resource from storage.
+     *
+     * @param  \App\Etf  $etf
+     * @return \Illuminate\Http\Response
+     */
+    public function destroy(Etf $etf)
+    {
+        //
+    }
+}
---
+++ app/Http/Controllers/KindAllotsController.php
@@ -0,0 +1,85 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use App\KindAllot;
+use Illuminate\Http\Request;
+
+class KindAllotsController extends Controller
+{
+    /**
+     * Display a listing of the resource.
+     *
+     * @return \Illuminate\Http\Response
+     */
+    public function index()
+    {
+        //
+    }
+
+    /**
+     * Show the form for creating a new resource.
+     *
+     * @return \Illuminate\Http\Response
+     */
+    public function create()
+    {
+        //
+    }
+
+    /**
+     * Store a newly created resource in storage.
+     *
+     * @param  \Illuminate\Http\Request  $request
+     * @return \Illuminate\Http\Response
+     */
+    public function store(Request $request)
+    {
+        //
+    }
+
+    /**
+     * Display the specified resource.
+     *
+     * @param  \App\KindAllot  $kindAllot
+     * @return \Illuminate\Http\Response
+     */
+    public function show(KindAllot $kindAllot)
+    {
+        //
+    }
+
+    /**
+     * Show the form for editing the specified resource.
+     *
+     * @param  \App\KindAllot  $kindAllot
+     * @return \Illuminate\Http\Response
+     */
+    public function edit(KindAllot $kindAllot)
+    {
+        //
+    }
+
+    /**
+     * Update the specified resource in storage.
+     *
+     * @param  \Illuminate\Http\Request  $request
+     * @param  \App\KindAllot  $kindAllot
+     * @return \Illuminate\Http\Response
+     */
+    public function update(Request $request, KindAllot $kindAllot)
+    {
+        //
+    }
+
+    /**
+     * Remove the specified resource from storage.
+     *
+     * @param  \App\KindAllot  $kindAllot
+     * @return \Illuminate\Http\Response
+     */
+    public function destroy(KindAllot $kindAllot)
+    {
+        //
+    }
+}
